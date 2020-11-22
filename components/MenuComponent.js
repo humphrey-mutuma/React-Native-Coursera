a494140f9ea1df75051f8a3a2c85917de266ea2c
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-11-22 12:25:55 +0300

    components p2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
@@ -12,6 +12,7 @@ function Menu(props) {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
+                    onPress = {() => props.onPress(item.id)}
                     leftAvatar={{ source: require('./images/uthappizza.png')}}
                   />
         );
